b50831cad2c6b56301b6fa5be009d60547e8d1f5
3ricj <ericjohanson@gmail.com>
2023-08-11 16:11:41 -0700

    initial checkin

---
+++ ContraDrift/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace ContraDrift
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PID_Setting_Kd_RA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PID_Setting_Ki_RA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PID_Setting_Kp_RA = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PID_Setting_Kd_DEC = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.PID_Setting_Ki_DEC = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.PID_Setting_Kp_DEC = new System.Windows.Forms.TextBox();
+            this.RA = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Telescope";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(198, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(289, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(37, 120);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(702, 93);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Watch Folder";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(198, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(289, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(33, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 26);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(37, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(702, 91);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "ASCOM";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(47, 372);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.RA);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.PID_Setting_Kd_DEC);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.PID_Setting_Ki_DEC);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.PID_Setting_Kp_DEC);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.PID_Setting_Kd_RA);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.PID_Setting_Ki_RA);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.PID_Setting_Kp_RA);
+            this.groupBox3.Location = new System.Drawing.Point(37, 219);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(702, 127);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "PID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(310, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Kd=";
+            // 
+            // PID_Setting_Kd_RA
+            // 
+            this.PID_Setting_Kd_RA.Location = new System.Drawing.Point(336, 37);
+            this.PID_Setting_Kd_RA.Name = "PID_Setting_Kd_RA";
+            this.PID_Setting_Kd_RA.Size = new System.Drawing.Size(62, 20);
+            this.PID_Setting_Kd_RA.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(198, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ki=";
+            // 
+            // PID_Setting_Ki_RA
+            // 
+            this.PID_Setting_Ki_RA.Location = new System.Drawing.Point(224, 37);
+            this.PID_Setting_Ki_RA.Name = "PID_Setting_Ki_RA";
+            this.PID_Setting_Ki_RA.Size = new System.Drawing.Size(62, 20);
+            this.PID_Setting_Ki_RA.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kp=";
+            // 
+            // PID_Setting_Kp_RA
+            // 
+            this.PID_Setting_Kp_RA.Location = new System.Drawing.Point(108, 37);
+            this.PID_Setting_Kp_RA.Name = "PID_Setting_Kp_RA";
+            this.PID_Setting_Kp_RA.Size = new System.Drawing.Size(62, 20);
+            this.PID_Setting_Kp_RA.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(310, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Kd=";
+            // 
+            // PID_Setting_Kd_DEC
+            // 
+            this.PID_Setting_Kd_DEC.Location = new System.Drawing.Point(336, 84);
+            this.PID_Setting_Kd_DEC.Name = "PID_Setting_Kd_DEC";
+            this.PID_Setting_Kd_DEC.Size = new System.Drawing.Size(62, 20);
+            this.PID_Setting_Kd_DEC.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(198, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Ki=";
+            // 
+            // PID_Setting_Ki_DEC
+            // 
+            this.PID_Setting_Ki_DEC.Location = new System.Drawing.Point(224, 84);
+            this.PID_Setting_Ki_DEC.Name = "PID_Setting_Ki_DEC";
+            this.PID_Setting_Ki_DEC.Size = new System.Drawing.Size(62, 20);
+            this.PID_Setting_Ki_DEC.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(82, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Kp=";
+            // 
+            // PID_Setting_Kp_DEC
+            // 
+            this.PID_Setting_Kp_DEC.Location = new System.Drawing.Point(108, 84);
+            this.PID_Setting_Kp_DEC.Name = "PID_Setting_Kp_DEC";
+            this.PID_Setting_Kp_DEC.Size = new System.Drawing.Size(62, 20);
+            this.PID_Setting_Kp_DEC.TabIndex = 6;
+            // 
+            // RA
+            // 
+            this.RA.AutoSize = true;
+            this.RA.Location = new System.Drawing.Point(33, 40);
+            this.RA.Name = "RA";
+            this.RA.Size = new System.Drawing.Size(22, 13);
+            this.RA.TabIndex = 12;
+            this.RA.Text = "RA";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(33, 87);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "DEC";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 758);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "ContraDrift";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox PID_Setting_Kd_RA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PID_Setting_Ki_RA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PID_Setting_Kp_RA;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox PID_Setting_Kd_DEC;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox PID_Setting_Ki_DEC;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox PID_Setting_Kp_DEC;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label RA;
+    }
+}
+
---
+++ ContraDrift/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ASCOM.DriverAccess;
+using ASCOM.DeviceInterface;
+using System.IO;
+using PinPoint;
+using System.Runtime.InteropServices;
+using NLog;
+
+
+namespace ContraDrift
+{
+    public partial class Form1 : Form
+    {
+        Logger log = LogManager.GetCurrentClassLogger();
+        static ContraDrift.Properties.Settings settings = Properties.Settings.Default;
+        BackgroundWorker worker = new BackgroundWorker();
+        FileSystemWatcher watcher = new FileSystemWatcher();
+        TaskFactory tFactory = new TaskFactory();
+        private Telescope telescope;
+        private bool FirstImage = true;
+
+        private System.Diagnostics.Stopwatch PID_dt; // delta time from last image, double seconds. 
+        private double PID_previous_error_RA = 0;
+
+        private double PID_propotional_RA = 0;
+        private double PID_integral_RA = 0;
+        private double PID_error_RA = 0;
+        private double PID_derivative_RA = 0;
+
+        private double PID_previous_error_DEC = 0;
+        private double PID_propotional_DEC = 0;
+        private double PID_integral_DEC = 0;
+        private double PID_error_DEC = 0;
+        private double PID_derivative_DEC = 0;
+        private DateTime LastExposure; 
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            LogManager.Setup().LoadConfigurationFromFile("nlog.config");
+
+            log = LogManager.GetCurrentClassLogger();
+
+            textBox1.Text = settings.TelescopeProgId;
+            textBox2.Text = settings.WatchFolder;
+            PID_Setting_Kp_RA.Text = settings.PID_Setting_Kp_RA.ToString();
+            PID_Setting_Ki_RA.Text = settings.PID_Setting_Ki_RA.ToString();
+            PID_Setting_Kd_RA.Text = settings.PID_Setting_Kd_RA.ToString();
+            PID_Setting_Kp_DEC.Text = settings.PID_Setting_Kp_DEC.ToString();
+            PID_Setting_Ki_DEC.Text = settings.PID_Setting_Ki_DEC.ToString();
+            PID_Setting_Kd_DEC.Text = settings.PID_Setting_Kd_DEC.ToString();
+
+            settings.Status = "Stopped";
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ASCOM.Utilities.Chooser chsr = new ASCOM.Utilities.Chooser();
+            //chsr.DeviceType = "Telescope";
+            //string scopeID = chsr.Choose();
+            //            scope = new scopeID;
+
+            ASCOM.Utilities.Chooser selector;
+            selector = new ASCOM.Utilities.Chooser();
+            selector.DeviceType = "Telescope";
+            settings.TelescopeProgId = selector.Choose(settings.TelescopeProgId);
+            settings.Save();
+            textBox1.Text = settings.TelescopeProgId;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox2.Text = folderBrowserDialog1.SelectedPath;
+            settings.WatchFolder = textBox2.Text;
+            settings.Save();
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (settings.Status == "Running")
+            {
+                // stop
+                settings.Status = "Stopped";
+                button3.Text = "Start";
+                button1.Enabled = true;
+                button2.Enabled = true;
+
+                watcher.EnableRaisingEvents = false;
+                //watcher.Dispose();
+                telescope.Connected = false;
+                telescope.Dispose();
+
+
+
+            }
+            else
+            {
+                // start
+                settings.Status = "Running";
+                button3.Text = "Stop";
+                button1.Enabled = false;
+                button2.Enabled = false;
+
+                telescope = new Telescope(settings.TelescopeProgId);
+                telescope.Connected = true;
+                telescope.Tracking = true;
+
+                double alt, az;
+
+                alt = telescope.Altitude;
+                az = telescope.Azimuth;
+                //Console.WriteLine("alt=" + alt + "\t az=" + az);
+                log.Debug("alt = " + alt + "\t az = " + az);
+
+                worker.DoWork += new DoWorkEventHandler(worker_backgroundProcess);
+                worker.RunWorkerAsync();
+                watcher.Path = textBox2.Text;
+                watcher.NotifyFilter = NotifyFilters.Attributes
+                                     | NotifyFilters.CreationTime
+                                     | NotifyFilters.DirectoryName
+                                     | NotifyFilters.FileName
+                                     | NotifyFilters.LastAccess
+                                     | NotifyFilters.LastWrite
+                                     | NotifyFilters.Security
+                                     | NotifyFilters.Size;
+
+                watcher.Created += new FileSystemEventHandler(ProcessNewFits);
+                watcher.Renamed += new RenamedEventHandler(ProcessNewFits);
+                watcher.Changed += new FileSystemEventHandler(ProcessNewFits);
+
+                watcher.Filter = "*.fits";
+                watcher.EnableRaisingEvents = true;
+                Console.WriteLine("Watcher Enabled");
+            }
+        }
+
+        void worker_backgroundProcess(object sender, DoWorkEventArgs e)
+        {
+            // for any background processing here... outside of new image processing. 
+        }
+
+        void ProcessNewFits(object sender, FileSystemEventArgs e)
+        {
+            tFactory.StartNew(() => {
+
+            double PlateRa = 0, PlateDec = 0, PlateExposureTime;
+            DateTime PlateLocaltime;
+            double ScopeRa, ScopeDec;
+            double ScopeRaRate, ScopeDecRate;
+
+            string InputFilename = e.FullPath;
+            Console.WriteLine("New File: " + InputFilename);
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            Plate p = new Plate();
+            try { p.AttachFITS(InputFilename); } catch (Exception ex) { Console.WriteLine(ex); }
+
+            p.ArcsecPerPixelHoriz = (Convert.ToDouble(p.ReadFITSValue("XPIXSZ")) / Convert.ToDouble(p.ReadFITSValue("FOCALLEN"))) * 206.2648062;
+            p.ArcsecPerPixelVert = (Convert.ToDouble(p.ReadFITSValue("YPIXSZ")) / Convert.ToDouble(p.ReadFITSValue("FOCALLEN"))) * 206.2648062;
+            p.RightAscension = p.TargetRightAscension;
+            p.Declination = p.TargetDeclination;
+            p.Catalog = (CatalogType)11;
+            p.CatalogPath = "J:/UCAC4";
+
+            try
+            {
+                p.Solve();
+                Console.Write(p.RightAscension + ",");
+                Console.Write(p.Declination + ",");
+                PlateRa = p.RightAscension;
+                PlateDec = p.Declination;
+                PlateLocaltime = p.ExposureStartTime;
+                PlateExposureTime = p.ExposureInterval;
+                
+            }
+            catch (Exception ex) { Console.WriteLine(ex); }
+
+
+            stopwatch.Stop();
+            Console.WriteLine((float)stopwatch.ElapsedMilliseconds / 1000);
+            p.DetachFITS();
+            _ = Marshal.ReleaseComObject(p); // important or the com object leaks memory
+
+            telescope.SlewToCoordinates(20.7333995798772, 45.3667092514744); //a blocking call for testing; 
+
+            ScopeRa = telescope.RightAscension;
+            ScopeDec = telescope.Declination;
+            ScopeRaRate = telescope.RightAscensionRate / 15; //  arcsec to RA Sec per sidereal second divide by 15.  
+            ScopeDecRate = telescope.DeclinationRate;
+
+            Console.WriteLine("ScopeRa: " + ScopeRa);
+            Console.WriteLine("ScopeDec: " + ScopeDec);
+            Console.WriteLine("ScopeRaRate: " + ScopeRaRate);
+            Console.WriteLine("ScopeDecRate: " + ScopeDecRate);
+
+                // LastExposure = PlateLocaltime.AddSeconds(PlateExposureTime / 2);
+                // TODO:  Change how DT is calucated. 
+
+
+
+                // PID control for RA
+                PID_error_RA = ScopeRa - PlateRa;
+                PID_propotional_RA = PID_error_RA;
+                PID_integral_RA = PID_integral_RA + PID_error_RA * ((float)PID_dt.ElapsedMilliseconds);
+                PID_derivative_RA = (PID_error_RA - PID_previous_error_RA) / ((float)PID_dt.ElapsedMilliseconds);
+                double new_RA_rate = settings.PID_Setting_Kp_RA * PID_propotional_RA + settings.PID_Setting_Ki_RA * PID_integral_RA + settings.PID_Setting_Kd_RA * PID_derivative_RA;
+
+                Console.WriteLine("PID_propotional_RA: " + PID_propotional_RA);
+                Console.WriteLine("PID_integral_RA: " + PID_integral_RA);
+                Console.WriteLine("PID_derivative_RA: " + PID_derivative_RA);
+                Console.WriteLine("new_RA_rate: " + new_RA_rate);
+
+
+                // PID control for DEC
+                PID_error_DEC = ScopeDec - PlateDec;
+                PID_propotional_DEC = PID_error_DEC;
+                PID_integral_DEC = PID_integral_DEC + PID_error_DEC * ((float)PID_dt.ElapsedMilliseconds);
+                PID_derivative_DEC = (PID_error_DEC - PID_previous_error_DEC) / ((float)PID_dt.ElapsedMilliseconds);
+                double new_DEC_rate = settings.PID_Setting_Kp_DEC * PID_propotional_DEC + settings.PID_Setting_Ki_DEC * PID_integral_DEC + settings.PID_Setting_Kd_DEC * PID_derivative_DEC;
+
+
+                Console.WriteLine("PID_propotional_DEC: " + PID_propotional_DEC);
+                Console.WriteLine("PID_integral_DEC: " + PID_integral_DEC);
+                Console.WriteLine("PID_derivative_DEC: " + PID_derivative_DEC);
+                Console.WriteLine("new_DEC_rate: " + new_DEC_rate);
+
+
+                // TODO: Check that the mount is tracking, and if telescope.RARateIsSettable is true. 
+
+                telescope.RightAscensionRate = new_RA_rate;
+                telescope.DeclinationRate = new_DEC_rate;
+
+                PID_dt.Restart(); // this restarts the stopwatch so dt has the correct value on the next image. 
+
+
+              }).ContinueWith(t =>
+              {
+                  if (t.IsFaulted)
+                  {
+                    // EXCEPTION IF THREAD IS FAULT
+                    throw t.Exception;
+                  }
+                  {
+                    //PROCESS IMAGES AND DISPLAY
+                }
+              });
+
+    } 
+    }
+        
+}
+    
+
